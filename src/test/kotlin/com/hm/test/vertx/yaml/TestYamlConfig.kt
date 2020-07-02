6b6591a2eff8b09186c5f956259afb450a31a259
nheise <Niklas.Heise@bkk-vbu.de>
2020-07-02 16:36:22 +0200

    make properties out of json object config

--- src/test/kotlin/com/hm/test/vertx/yaml/TestYamlConfig.kt
+++ src/test/kotlin/com/hm/test/vertx/yaml/TestYamlConfig.kt
@@ -10,6 +10,7 @@ import io.vertx.kotlin.core.json.json
 import io.vertx.kotlin.core.json.obj
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import java.util.*
 
 @ExtendWith(VertxExtension::class)
 class TestYamlConfig {
@@ -30,11 +31,15 @@ class TestYamlConfig {
     val retriever = ConfigRetriever.create(vertx, options)
 
     retriever.getConfig {
-      val config = it.result()
+      val config = it.result().getJsonObject("level1").getJsonObject("props")
+      //println(config)
 
+      val props = Properties()
+      props.putAll(config.map)
+
+      assert(props.getProperty("key1") == "value1")
+      testContext.completeNow()
     }
-
-    testContext.completeNow()
   }
 
 }
