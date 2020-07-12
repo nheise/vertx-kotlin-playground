ebf6676c2439bb4cde4813d5cba58d22f84e648a
nheise <Niklas.Heise@bkk-vbu.de>
2020-07-12 20:07:38 +0200

    map config into data class

--- src/test/kotlin/com/hm/test/config/yaml/TestYamlConfig.kt
+++ src/test/kotlin/com/hm/test/config/yaml/TestYamlConfig.kt
@@ -1,13 +1,17 @@
-package com.hm.test.vertx.yaml
+package com.hm.test.config.yaml
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import io.vertx.config.ConfigRetriever
 import io.vertx.core.Vertx
+import io.vertx.core.json.JsonObject
 import io.vertx.junit5.VertxExtension
 import io.vertx.junit5.VertxTestContext
 import io.vertx.kotlin.config.configRetrieverOptionsOf
 import io.vertx.kotlin.config.configStoreOptionsOf
+import io.vertx.kotlin.core.json.get
 import io.vertx.kotlin.core.json.json
 import io.vertx.kotlin.core.json.obj
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import java.util.*
@@ -37,9 +41,38 @@ class TestYamlConfig {
       val props = Properties()
       props.putAll(config.map)
 
-      assert(props.getProperty("key1") == "value1")
+      assertEquals("value1", props.getProperty("key1"))
       testContext.completeNow()
     }
   }
 
+  @Test
+  fun loadConfigurationAsObject(vertx: Vertx, testContext: VertxTestContext) {
+
+    val configOptions = configStoreOptionsOf(
+      type = "file",
+      format = "yaml",
+      config = json {
+        obj("path" to "src/test/resources/test-config.yaml")
+      })
+
+    val options = configRetrieverOptionsOf(
+      stores = listOf(configOptions))
+
+    val retriever = ConfigRetriever.create(vertx, options)
+
+    retriever.getConfig {
+      val config: JsonObject = it.result()
+      val propsConfig = config.get<JsonObject>("level1").get<JsonObject>("props").mapTo(PropsConfig::class.java)
+
+      assertEquals("value1", propsConfig.key1)
+
+      testContext.completeNow()
+    }
+  }
+
+  data class PropsConfig(
+    @JsonProperty("key1") val key1: String,
+    @JsonProperty("key2") val key2: String
+  )
 }
