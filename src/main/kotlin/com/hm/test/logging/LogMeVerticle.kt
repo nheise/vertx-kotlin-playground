cfb0010977525d84324994cb4d083a8ca8259bb7
nheise <Niklas.Heise@bkk-vbu.de>
2020-07-31 13:28:02 +0200

    logging in request context

---
+++ src/main/kotlin/com/hm/test/logging/LogMeVerticle.kt
@@ -0,0 +1,50 @@
+package com.hm.test.logging
+
+import io.vertx.ext.web.Router
+import io.vertx.ext.web.RoutingContext
+import io.vertx.kotlin.core.http.listenAwait
+import io.vertx.kotlin.coroutines.CoroutineVerticle
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.slf4j.MDC
+
+class LogMeVerticle : CoroutineVerticle() {
+
+  private val logger = LoggerFactory.getLogger(LogMeVerticle::class.java)
+
+  override suspend fun start() {
+    val router = Router.router(vertx)
+
+    router.get("/logme")
+      .handler(::addRequestIdIntoLogContext)
+      .handler {
+        AppLog.info("Got request")
+        it.response().end()
+      }
+      .failureHandler {
+        logger.error("Unexpected error", it.failure())
+        it.next()
+      }
+      .handler(::clearLogContext)
+
+    vertx
+      .createHttpServer()
+      .requestHandler(router)
+      .listenAwait(8080, "localhost")
+  }
+}
+
+object AppLog : Logger by LoggerFactory.getLogger("Application Logger")
+
+fun addRequestIdIntoLogContext(ctx: RoutingContext) {
+  MDC.put("requestId", ctx.requestId)
+  ctx.next()
+}
+
+fun clearLogContext(ctx: RoutingContext) {
+  MDC.clear()
+  println("Clear log context")
+}
+
+val RoutingContext.requestId: String
+  get() = request().getHeader("X_REQUEST_ID") ?: "no_request_id_present"
---
+++ src/test/kotlin/com/hm/test/logging/LogMeVerticleTest.kt
@@ -0,0 +1,56 @@
+package com.hm.test.logging
+
+import io.vertx.core.Vertx
+import io.vertx.ext.web.client.WebClient
+import io.vertx.ext.web.codec.BodyCodec
+import io.vertx.junit5.VertxExtension
+import io.vertx.junit5.VertxTestContext
+import io.vertx.kotlin.core.deployVerticleAwait
+import io.vertx.kotlin.ext.web.client.sendAwait
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+@ExtendWith(VertxExtension::class)
+internal class LogMeVerticleTest {
+
+  @BeforeEach
+  fun setUp(vertx: Vertx, testContext: VertxTestContext) {
+    runBlocking {
+      vertx.deployVerticleAwait(LogMeVerticle())
+      testContext.completeNow()
+    }
+  }
+
+  @Test
+  fun checkShareServiceOutput(vertx: Vertx, testContext: VertxTestContext) {
+    runBlocking {
+      val webClient = WebClient.create(vertx)
+
+      repeatConcurrent(10) {
+        var resp = webClient.get(8080, "localhost", "/logme")
+          .`as`(BodyCodec.string())
+          .putHeader("X_REQUEST_ID", "$it")
+          .sendAwait()
+        assertEquals(200, resp.statusCode())
+      }
+
+      testContext.completeNow()
+    }
+  }
+
+  private suspend fun repeatConcurrent(steps: Int, action: suspend (iteration: Int) -> Unit) {
+    coroutineScope {
+      (1..steps).forEach {
+        launch {
+          action(it)
+        }
+      }
+    }
+  }
+
+}
